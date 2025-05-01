2a7499c901fe74f11a5d21d31b00b2f28189e699
nakisara01 <nakisara01@ajou.ac.kr>
2025-05-01 10:08:13 +0900

    Initial Commit

---
+++ yumin/yuminApp.swift
@@ -0,0 +1,17 @@
+//
+//  yuminApp.swift
+//  yumin
+//
+//  Created by 나현흠 on 5/1/25.
+//
+
+import SwiftUI
+
+@main
+struct yuminApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
